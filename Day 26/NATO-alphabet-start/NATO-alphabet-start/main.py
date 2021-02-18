4e08c96743c4de0f76e0716a1c11c8a32f5ef880
Orvil D'silva <orvildsilva@gmail.com>
2021-02-18 23:14:23 +0530

    day 26

--- Day 26/NATO-alphabet-start/NATO-alphabet-start/main.py
+++ Day 26/NATO-alphabet-start/NATO-alphabet-start/main.py
@@ -10,7 +10,6 @@ letter_dict ={row['letter']: row['code'] for (index,row) in data.iterrows()}
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 user_input = input("Enter the word:").upper()
-user_input_list = [letter for letter in user_input]
 
-result = [letter_dict[letter] for letter in user_input_list]
+result = [letter_dict[letter] for letter in user_input]
 print(result)
