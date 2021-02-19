f743a443af65a2c1ec46c18d60b45a447ce026f0
Orvil D'silva <orvildsilva@gmail.com>
2021-02-19 16:08:04 +0530

    Day 27

---
+++ Day 27/trial.py
@@ -0,0 +1,21 @@
+from tkinter import *
+
+window = Tk()
+window.title("Haha GUI")
+window.minsize(width=600, height=600)
+
+label = Label(text="My label")
+label.pack()
+
+
+def label_shown():
+    label.config(text=input.get())
+
+
+button = Button(text="click me", command=label_shown)
+button.pack()
+
+input = Entry(width=10)
+input.pack()
+
+window.mainloop()
