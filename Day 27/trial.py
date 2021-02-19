f79cddc154fbef02244e532e5383db9065288339
orvil1026 <orvildsilva@gmail.com>
2021-02-19 22:44:16 +0530

    day 27

--- Day 27/trial.py
+++ Day 27/trial.py
@@ -3,9 +3,10 @@ from tkinter import *
 window = Tk()
 window.title("Haha GUI")
 window.minsize(width=600, height=600)
+window.config(padx=100, pady=100)
 
 label = Label(text="My label 1")
-label.pack()
+label.grid(column=0,row=0)
 
 
 def label_shown():
@@ -13,9 +14,12 @@ def label_shown():
 
 
 button = Button(text="click me", command=label_shown)
-button.pack()
+button.grid(row=1, column=1)
+
+button2 = Button(text="new_button", command=label_shown)
+button2.grid(row=0, column=2)
 
 input = Entry(width=10)
-input.pack()
+input.grid(row=2, column=3)
 
 window.mainloop()
